9fccdfbded7366cfe10d87617e137215a568c56b
Abhijit <abhijit.khanna@live.com>
2017-10-01 18:59:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 var playlist = {'Slowdive':'Alison'}
 
 function updatePlaylist (playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
+=======
+var playslist = {'Peter Bjorn & John': 'Young Folks'}
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  playlist.artistName = songTitle;
+>>>>>>> 24ad3bb5388f5e1bcf3dd58b1635586b0b9d95d6
 
   return playlist
 
@@ -9,7 +16,11 @@ function updatePlaylist (playlist, artistName, songTitle) {
 
 function removeFromPlaylist (playlist, artistName) {
 
+<<<<<<< HEAD
   delete playlist[artistName]
+=======
+  delete playlist.artistName
+>>>>>>> 24ad3bb5388f5e1bcf3dd58b1635586b0b9d95d6
 
   return playlist
 
